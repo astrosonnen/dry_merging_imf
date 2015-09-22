1bb484c241b24262e25fa53c2eb8bfe83324fcb3
sonnen <alessandro.sonnenfeld@ipmu.jp>
2015-09-22 18:03:46 +0900

    mostly code cosmetics

--- cgsconstants.py
+++ cgsconstants.py
@@ -1,4 +1,10 @@
 
+H0 = 70.
+omegaM = 0.3
+omegaL = 0.7
+omegak = 0.
+
+cosmo = {'omega_M_0': omegaM, 'omega_lambda_0': omegaL, 'omega_k_0': 0., 'h': H0/100.}
 
 AA = 10.**(-8)
 
@@ -17,8 +23,6 @@ pc = 3.08568025e18
 kpc = pc*10.**3
 
 Mpc = pc*10.**6
-
-H0 = 70*100000./Mpc
 
 yr = 365*3600*24.
 
--- recipes.py
+++ recipes.py
@@ -1,59 +1,60 @@
 from sonnentools.cgsconstants import *
 import numpy as np
-import cosmology
+from cosmolopy import density, distance
 from scipy.interpolate import splrep,splev
 
 Nz = 1001
-z_grid = np.linspace(0.,5.,Nz)
+z_grid = np.linspace(0., 5., Nz)
 logt_grid = 0.*z_grid
 rhoc_grid = 0.*z_grid
 
-for i in range(1,Nz):
-    logt_grid[i] = np.log10(cosmology.lookback(z_grid[i])) - 9.
-    rhoc_grid[i] = cosmology.rhoc(z_grid[i])
+for i in range(1, Nz):
+    logt_grid[i] = np.log10(distance.lookback_time(z_grid[i]), **cosmo) - 9.
+    rhoc_grid[i] = density.cosmo_densities(**cosmo)*distance.e_z(z_grid[i], **cosmo)
 
-z_spline = splrep(logt_grid[1:],z_grid[1:])
-logt_spline = splrep(z_grid[1:],logt_grid[1:])
-rhoc_spline = splrep(z_grid[1:],rhoc_grid[1:])
+z_spline = splrep(logt_grid[1:], z_grid[1:])
+logt_spline = splrep(z_grid[1:], logt_grid[1:])
+rhoc_spline = splrep(z_grid[1:], rhoc_grid[1:])
 
 def z_form_mstar_func(lmstar):
-    return splev(0.427 + 0.053*lmstar,z_spline)
-    #return 1.9856 + 0.8713*(lmstar -11.) + 0.3451*(lmstar - 11.)**2
+    return splev(0.427 + 0.053*lmstar, z_spline)
 
-def z_form_sigma_func(lsigma):
-    return splev(0.46 + 0.238*lsigma,z_spline)
 
-def dtform_func(lsigma):
-    return 10.**(3.44 - 1.68*lsigma + 9.)
+def z_form_vdisp_func(lvdisp):
+    return splev(0.46 + 0.238*lvdisp,z_spline)
+
+
+def dtform_func(lvdisp):
+    return 10.**(3.44 - 1.68*lvdisp + 9.)
+
 
 def lmstar_zfunc(z):
     logt = splev(z,logt_spline)
     return logt/0.053 - 0.427/0.053
 
+
 def SigmaSF(mstar,re,dt):
     return 0.5*mstar/(np.pi*re**2)/dt
 
 
-def sigma_from_mstar(lmstar): #from Thomas et al. 2005 eq. (2)
+def vdisp_mstar_rel(lmstar): #from Thomas et al. 2005 eq. (2)
     return 10.**((lmstar - 0.63)/4.52)
 
 
-def re_from_mstar(lmstar): #from Newman et al. 2012, SDSS bin
+def re_mstar_rel(lmstar): #from Newman et al. 2012, SDSS bin
     return 10.**(0.54 + 0.57*(lmstar - 11.))
 
 
-def limf_func_CvD12(mstar,re,dt,a=0.1,b=0.3):
-    return a + b*(np.log10(SigmaSF(mstar,re,dt)) - 1.)
-    
-
-def limf_func_rhoc(z_form,a=0.3,b=1.):
-    #rhoc = cosmology.rhoc(z_form_func(lmstar))
-    rhoc = splev(z_form,rhoc_spline)
-    return a + b*(np.log10(rhoc) + 28.)
+def limf_func_CvD12(mstar, re, dt, coeff=(0.1,0.3)):
+    return coeff[0] + coeff[1]*(np.log10(SigmaSF(mstar, re, dt)) - 1.)
 
 
+def limf_func_rhoc(z_form, coeff=(0.3, 1.0)):
+    rhoc = splev(z_form, rhoc_spline)
+    return coeff[0] + coeff[1]*(np.log10(rhoc) + 28.)
 
-def central_imf(galaxy, recipe='sigma', coeff=(0.1,0.3)):
+
+def central_imf(galaxy, recipe='SigmaSF', coeff=(0.1,0.3)):
     """
 
     :param galaxy: ETG class object
@@ -63,14 +64,16 @@ def central_imf(galaxy, recipe='sigma', coeff=(0.1,0.3)):
 
     This function assigns an IMF to the stellar population of a galaxy at the time of star formation.
     Two recipes for assigning the IMF are implemented.
-    'density': We use
+    'SigmaSF': The IMF normalization scales with the surface density of star formation, following the idea of
+    Conroy and van Dokkum (2012).
+    'density': The IMF normalization scales with the critical density of the Universe at the time of star formation
     """
 
     if recipe == 'SigmaSF':
         return 10.**limf_func_CvD12(galaxy.mstar_chab[-1], galaxy.re[-1], galaxy.dtform, coeff[0], coeff[1])
 
     elif recipe == 'density':
-        return 10.**limf_func_rhoc(galaxy.z_form, coeff[0], coeff[1])
+        return 10.**limf_func_rhoc(galaxy.z_form, coeff)
 
     else:
         raise ValueError("recipe must be one between 'SigmaSF' and 'density'.")
@@ -79,14 +82,14 @@ def central_imf(galaxy, recipe='sigma', coeff=(0.1,0.3)):
 def satellite_imf(lmstar, recipe='SigmaSF', coeff=(0.1,0.3)):
 
     if recipe == 'SigmaSF':
-        dtform = dtform_func(np.log10(sigma_from_mstar(lmstar)))
-        re = re_from_mstar(lmstar)
-        return 10.**limf_func_CvD12(10.**lmstar,re,dtform,coeff[0],coeff[1])
+        dtform = dtform_func(np.log10(vdisp_mstar_rel(lmstar)))
+        re = re_mstar_rel(lmstar)
+        return 10.**limf_func_CvD12(10.**lmstar, re, dtform, coeff[0], coeff[1])
 
     elif recipe == 'mstar':
         z_form = z_form_mstar_func(lmstar)
-        return 10.**limf_func_rhoc(z_form,coeff[0],coeff[1])
+        return 10.**limf_func_rhoc(z_form, coeff[0], coeff[1])
 
     else:
-         raise ValueError("recipe must be one between 'sigma' and 'mstar'.")
+         raise ValueError("recipe must be one between 'SigmaSF' and 'mstar'.")
 
